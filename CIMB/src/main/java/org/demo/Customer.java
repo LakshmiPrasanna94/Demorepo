44f9fe7cf7b75e6c4f23592dae609b918c7cb301
bpmsAdmin <>
2015-10-12 11:31:43 +0530

    {/CIMB/src/main/java/org/demo/Customer.java}

--- CIMB/src/main/java/org/demo/Customer.java
+++ CIMB/src/main/java/org/demo/Customer.java
@@ -17,6 +17,12 @@ public class Customer implements java.io.Serializable
 
    private java.lang.String address;
 
+   private java.lang.Integer mnumber;
+
+   private java.lang.Boolean female;
+
+   private java.lang.Boolean male;
+
    public Customer()
    {
    }
@@ -61,13 +67,48 @@ public class Customer implements java.io.Serializable
       this.address = address;
    }
 
+   public java.lang.Integer getMnumber()
+   {
+      return this.mnumber;
+   }
+
+   public void setMnumber(java.lang.Integer mnumber)
+   {
+      this.mnumber = mnumber;
+   }
+
+   public java.lang.Boolean getFemale()
+   {
+      return this.female;
+   }
+
+   public void setFemale(java.lang.Boolean female)
+   {
+      this.female = female;
+   }
+
+   public java.lang.Boolean getMale()
+   {
+      return this.male;
+   }
+
+   public void setMale(java.lang.Boolean male)
+   {
+      this.male = male;
+   }
+
    public Customer(java.lang.Integer custid, java.lang.String custname,
-         java.lang.Integer age, java.lang.String address)
+         java.lang.Integer age, java.lang.String address,
+         java.lang.Integer mnumber, java.lang.Boolean female,
+         java.lang.Boolean male)
    {
       this.custid = custid;
       this.custname = custname;
       this.age = age;
       this.address = address;
+      this.mnumber = mnumber;
+      this.female = female;
+      this.male = male;
    }
 
 }
